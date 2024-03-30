4d3acbffdfc204c84c366b9012ce8652eb0c09f9
Kwuraa <kwuraa@gmail.com>
2024-03-29 21:41:47 -0300

    update of array exercises

--- JavaScript/aula-04/for.js
+++ JavaScript/aula-04/for.js
@@ -32,10 +32,85 @@ console.clear();
 
 //* Exemplo: percorrendo strings com for
 
-const nome = "Matheus";
+// const nome = "Matheus";
 
-for (let i = 0; i < nome.length; i++) {
-  console.log(nome[i]);
+// for (let i = 0; i < nome.length; i++) {
+//   console.log(nome[i]);
+// }
+
+const nota1 = 10;
+const nota2 = 5;
+const nota3 = 7;
+
+const notasDoAluno = [10, 5, 7];
+const pessoa = ["Matheus", 20, 1.52, true];
+
+console.log(notasDoAluno[0]);
+
+console.log(pessoa[2]);
+
+console.clear;
+
+console.log(pessoa.length);
+console.log(notasDoAluno.length);
+
+console.clear;
+
+// Fatiamento do array
+
+const numeros = [10, 20, 30, 55, 100, 200];
+
+console.log(numeros.slice(0, 2));
+console.log(numeros.slice(2));
+
+console.clear;
+
+// Adicionando um elemento no final do array
+numeros.push(0);
+console.log(numeros);
+
+console.clear;
+
+// Adicionar elemento no inicio do array
+numeros.unshift(1000);
+console.log(numeros);
+
+console.clear;
+// pop: serve para remover elementos no final do array (ultimo elemento)
+
+numeros.pop();
+console.log(numeros);
+// shift: remover primeiro elemento de um array
+
+numeros.shift();
+console.log(numeros);
+
+console.clear();
+// Sera que o elemento 20 esta no array?
+
+console.log(numeros.includes(20));
+console.log(numeros.includes(5));
+
+if (numeros.includes(20)) {
+  console.log("OPA!!! este numero existe dentro do array");
+} else {
+  console.log("Nao exite este numero no array");
 }
 
-// !! TEMPO DA AULA 26:42 !!!
+const existeONumero20 = numeros.includes(20);
+
+console.clear();
+
+// * IndexOf (Procura o primeiro elemento com o valor informado)
+
+const indexDoElemento200 = numeros.indexOf(200);
+
+console.log(indexDoElemento200);
+
+// * LastIndexOf (Procura o ultimo elemento com o valor informado )
+
+const indexDoElemento40 = numeros.lastIndexOf(200);
+
+console.log(indexDoElemento40);
+
+//! Tempo da aula 52:46
