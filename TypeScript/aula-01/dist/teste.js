60e410630f69d7994fea54cfe64b780b333b3df2
kwuraa <kwuraa@gmail.com>
2024-04-15 17:52:13 -0300

    TypeScript config

---
+++ TypeScript/aula-01/dist/teste.js
@@ -0,0 +1,4 @@
+"use strict";
+const numero1 = 10;
+const numero2 = 10;
+console.log(numero1 + numero2);
--- TypeScript/aula-01/teste.ts
+++ TypeScript/aula-01/teste.ts
@@ -1,4 +1,4 @@
-const numero1 = 4;
-const numero2 = 5;
+const numero1 = 10;
+const numero2 = 10;
 
 console.log(numero1 + numero2);
