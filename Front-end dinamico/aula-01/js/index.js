6dedbfcd90a789908d41636906b2813113d20b2c
kwuraa <kwuraa@gmail.com>
2024-04-22 23:32:33 -0300

    Browser API - document.getElements

---
+++ Front-end dinamico/aula-01/js/index.js
@@ -0,0 +1,14 @@
+// Selecionar elementos HTML da nossa pagina ultilizando o document
+
+// 1. Pelo nome da Tag
+const todosP = document.getElementsByTagName("p");
+
+console.log(todosP);
+
+// 2. Pelo nome da Classe
+
+const todosParagrafos = document.getElementsByClassName("paragrafo");
+
+console.log(todosParagrafos);
+
+// ! TIMING DA AULA 27:12
