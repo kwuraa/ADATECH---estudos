37e0d6441cc29b4549ecaa155a9396a9b1c89991
kwuraa <kwuraa@gmail.com>
2024-04-23 20:52:41 -0300

    Manipulando tags ultilizando DOM

--- Front-end dinamico/aula-01/js/index.js
+++ Front-end dinamico/aula-01/js/index.js
@@ -11,4 +11,59 @@ const todosParagrafos = document.getElementsByClassName("paragrafo");
 
 console.log(todosParagrafos);
 
-// ! TIMING DA AULA 27:12
+// 3. Pelo Nome (name)
+
+const emailInput = document.getElementsByName("email");
+
+console.log(emailInput);
+
+// 4. Pelo id da tag
+
+const imgSukuna = document.getElementById("sukuna-logo");
+
+console.log(imgSukuna);
+
+// 5. Query selector
+
+const img = document.querySelector("body > img#sukuna-logo");
+
+console.log(img);
+
+const paragrafos1 = document.querySelectorAll("p");
+
+console.log(paragrafos1);
+
+console.clear();
+
+// Acessando | Alterando o conteúdo HTML das tags
+
+const primeiroParagrafo = document.querySelector("p.paragrafo");
+console.log(primeiroParagrafo);
+
+console.log("Conteudo: ", primeiroParagrafo.textContent);
+console.log("innerHTML:", primeiroParagrafo.innerHTML);
+
+primeiroParagrafo.textContent = "Outra Coisa";
+primeiroParagrafo.innerHTML = "<strong> Outra coisa <strong>";
+
+console.clear();
+
+emailInput[0].value = "kawamura@mail.com";
+
+// Criando elementos na pagina HTML
+
+const listaUl = document.querySelector("ul#list");
+const litsLis = document.querySelectorAll("ul > li");
+
+const novaTagLi = document.createElement("li"); // Criando uma tag li vazia
+novaTagLi.textContent = "Segundo item"; // Coloando um texto dentro da tag li (textContent | innerHTML)
+
+console.log(novaTagLi);
+
+// listaUl.appendChild(novaTagLi);
+
+listaUl.insertBefore(novaTagLi, litsLis[1]);
+
+// removendo elementos do HTML
+
+listaUl.removeChild(novaTagLi);
