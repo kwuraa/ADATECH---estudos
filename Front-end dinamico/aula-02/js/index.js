2844e90c5a5edf9f0f74d0579be17f6059f01a07
kwuraa <kwuraa@gmail.com>
2024-04-24 12:24:13 -0300

    Escuta de eventos e estilos, manipulando css

---
+++ Front-end dinamico/aula-02/js/index.js
@@ -0,0 +1,60 @@
+const btnDiminuir = document.querySelector("#btn-diminuir");
+const btnAumentar = document.querySelector("#btn-aumentar");
+
+const contadorEl = document.querySelector("#contador");
+
+const input = document.querySelector("#input");
+
+btnAumentar.addEventListener("click", (event) => {
+  const valorAtual = Number(contadorEl.textContent);
+
+  contadorEl.textContent = valorAtual + 1;
+
+  btnAumentar.classList.add("btn");
+  btnDiminuir.classList.remove("btn");
+});
+
+btnDiminuir.addEventListener("click", (event) => {
+  const valorAtual = Number(contadorEl.textContent);
+
+  contadorEl.textContent = valorAtual - 1;
+
+  btnDiminuir.classList.add("btn");
+  btnAumentar.classList.remove("btn");
+});
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
+
+// Adicionando estilos inline no elemento contador
+
+contadorEl.style.color = "red";
+contadorEl.style.padding = "0 2rem";
+contadorEl.style.border = "1px solid #aaa";
+contadorEl.style.width = "50px";
+
+// Manipular classes
+
+console.log(btnDiminuir.classList);
+
+// btnDiminuir.classList.add("btn");
+// btnAumentar.classList.remove("btn");
+
+const themeBtn = document.querySelector("#theme");
+
+let darkTheme = false;
+
+themeBtn.addEventListener("click", () => {
+  darkTheme = !darkTheme;
+
+  const body = document.querySelector("body");
+
+  if (darkTheme) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+});
